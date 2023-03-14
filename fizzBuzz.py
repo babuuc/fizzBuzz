cd0c46b3d509b5dedd34ea9d8dbe085246ac30ac
babuuc <79839215+babuuc@users.noreply.github.com>
2023-03-14 10:26:05 +0100

    Create fizzBuzz.py

---
+++ fizzBuzz.py
@@ -0,0 +1,20 @@
+ileMod3 = 0
+ileMod5 = 0
+ileMod35 = 0
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        ileMod35 += 1
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        ileMod3 += 1
+        print("Fizz")
+    elif i % 5 == 0:
+        ileMod5 += 1
+        print("Buzz")
+    else:
+        print(i)
+
+print(f"Liczb podzielnych przez 3 jest: {ileMod3}")
+print(f"Liczb podzielnych przez 5 jest: {ileMod5}")
+print(f"Liczb podzielnych przez 3 i 5 jest: {ileMod35}")
